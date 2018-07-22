2f5b304620fd7c5bb228947087a4c514c821ff3f
LuanC03 <luan.bezerra@ccc.ufcg.edu.br>
2018-07-22 11:56:44 -0300

    Dividindo em modulos

--- bombardment.pl
+++ bombardment.pl
@@ -1,3 +1,7 @@
+﻿use_module(util).
+use_module(iteracao).
+use_module(exibicao).
+
 /* Regras para realizar tiros, manipulando o tabuleiro */
 
 atirar(Tabuleiro, NovoTabuleiro) :-
@@ -29,148 +33,6 @@ substituir([_|T], 0, X, [X|T]).
 substituir([H|T], Index, NewElement, [H|U]) :-
   Index1 is Index - 1, substituir(T, Index1, NewElement, U).
 
-
-/* EXIBIÇÃO */
-
-/* Impressão da visão que o jogador tem doh tabuleiro, omitindo navios */
-
-imprimeTabuleiroJogador(Tabuleiro) :-
-  write('############ BOMBARDMENT OF THE VIRUS ###########'),nl,nl,
-  write('    A   B   C   D   E   F   G   H   I   J   L   M'),nl,nl,
-  imprimeMatrizJogador(Tabuleiro, 0),
-  writeln('~ = Terreno | n = Area | X = TIRO PERDIDO').
-
-imprimeMatrizJogador([], _).
-imprimeMatrizJogador([H|T], Index) :-
-    write(Index), write('  '), imprimeLinhaJogador(H), nl,nl,
-    NewIndex is Index+1,
-    imprimeMatrizJogador(T, NewIndex).
-
-imprimeLinhaJogador([]).
-imprimeLinhaJogador([H|T]) :-
-  (H == '~', write('|_|');
-  H == n, write('|_|');
-  H == b, write('|_|');
-  H == a, write('|_|');
-  H == @, write('|_|');
-  H == &, write('')), write(' '),
-  imprimeLinhaJogador(T).
-
-imprimeTabuleiroReal(Tabuleiro) :-
-  write('############ BOMBARDMENT OF THE VIRUS ###########'),nl,nl,
-  write('    A   B   C   D   E   F   G   H   I   J   L   M'),nl,nl,
-  imprimeLinhas(Tabuleiro, 0),
-  writeln('~ = Terreno | n = Area | X = TIRO PERDIDO').
-
-imprimeLinhas([],_).
-imprimeLinhas([H|T],Index) :-
-	write(Index), write('  '), imprimeLinha(H), nl,nl,
-	NewIndex is Index+1,
-	imprimeLinhas(T,NewIndex).
-
-imprimeLinha([]).
-imprimeLinha([H|T]) :-
-	write(H), write('  '),
-	imprimeLinha(T).
-
-/* Impressões simples */
-
-acertou :-
-  write('ACERTOU!'), nl.
-
-errou :-
-  write('ERROU!'), nl.
-
-invalido :-
-  write('Você já atirou aqui! Atire em outro lugar.'), nl.
-
-selecione :-
-  write('Selecione as coordenadas de onde deseja atirar!'), nl.
-
-selecaoInvalida :-
-  write('Coordenadas inválidas. Tente novamente.'), nl.
-
-misseis(Qtd) :-
-  write('Você ainda tem '), write(Qtd), write(' mísseis.'), nl, nl.
-
-ultimoMissel :-
-  write('Resta apenas um míssel!'), nl, nl.
-
-misseisEsgotados :-
-  write('Seus mísseis acabaram!'), nl, nl.
-
-gameOver :-
-  write('---------------------------------------------------'), nl,
-  write(' Game Over!'), nl,
-  write('---------------------------------------------------').
-
-vitoria :-
-  write('---------------------------------------------------'), nl,
-  write('Você destruiu toda as Bases inimigas !! Parabéns, estrategista!'), nl,
-  write('---------------------------------------------------').
-
-/* Imprime uma mensagem na tela e lê um número da entrada */
-inserir_numero(Prompt, Numero) :-
-  write(Prompt),
-  write(': '),
-  read(Numero).
-
-
-/* Funções que verificam a existência de elementos em listas e matriz */
-
-contem([X|_], X).
-contem([_|T], X) :-
-  contem(T, X).
-
-existemNavios([H|_]) :- contem(H, n).
-existemNavios([_|T]) :- existemNavios(T).
-
-/* Funções de inserção de Bases no tabuleiro */
-
-inserirBPC(Tabuleiro, NovoTabuleiro):-
-    random(0,6,Linha),random(0,6,Coluna),random(0,2,Orientacao),
-    encontraSimboloNaMatriz(Tabuleiro, Linha, Coluna, _),Coluna2 is Coluna+1,Coluna3 is Coluna+2,Coluna4 is Coluna+3, Linha2 is Linha+1,Linha3 is Linha+2,Linha4 is Linha+3,
- (
- (Orientacao == 0) -> alteraValorNoTabuleiro(Tabuleiro, Linha, Coluna, b, Tabuleiro2),alteraValorNoTabuleiro(Tabuleiro2, Linha, Coluna2, b, Tabuleiro3),alteraValorNoTabuleiro(Tabuleiro3, Linha, Coluna3, b, Tabuleiro4),alteraValorNoTabuleiro(Tabuleiro4, Linha, Coluna4, b, NovoTabuleiro);
- (Orientacao == 1) -> alteraValorNoTabuleiro(Tabuleiro, Linha, Coluna, b, Tabuleiro2),alteraValorNoTabuleiro(Tabuleiro2, Linha2, Coluna, b, Tabuleiro3),alteraValorNoTabuleiro(Tabuleiro3, Linha3, Coluna, b, Tabuleiro4),alteraValorNoTabuleiro(Tabuleiro4, Linha4, Coluna, b, NovoTabuleiro)
-
- ).
-
-/*Tamanho 2*/
-inserirBTM(Tabuleiro, NovoTabuleiro):-
-    random(0,10,Linha),random(0,10,Coluna),random(0,2,Orientacao),
-    encontraSimboloNaMatriz(Tabuleiro, Linha, Coluna, Simbolo),Coluna2 is Coluna+1, Linha2 is Linha+1, encontraSimboloNaMatriz(Tabuleiro, Linha, Coluna2, Simbolo2),encontraSimboloNaMatriz(Tabuleiro, Linha2, Coluna, Simbolo3),
- (
- (Simbolo == ~), (Simbolo2 == ~), (Orientacao == 0) -> alteraValorNoTabuleiro(Tabuleiro, Linha, Coluna, n, Tabuleiro2),alteraValorNoTabuleiro(Tabuleiro2, Linha, Coluna2, n, NovoTabuleiro);
- (Simbolo == ~), (Simbolo3 == ~), (Orientacao == 1) -> alteraValorNoTabuleiro(Tabuleiro, Linha, Coluna, n, Tabuleiro2),alteraValorNoTabuleiro(Tabuleiro2, Linha2, Coluna, n, NovoTabuleiro);
- (Orientacao == 0),((Simbolo == n);(Simbolo2 == n))-> inserirCruiser(Tabuleiro, NovoTabuleiro);
- (Orientacao == 1),((Simbolo == n);(Simbolo3 == n))-> inserirCruiser(Tabuleiro, NovoTabuleiro)
-
- ).
-
-/*Tamanho 1*/
-inserirCT(Tabuleiro, NovoTabuleiro):-random(0,11,Linha),random(0,11,Coluna),
-    encontraSimboloNaMatriz(Tabuleiro, Linha, Coluna, Simbolo),
- (
- (Simbolo == ~) -> alteraValorNoTabuleiro(Tabuleiro, Linha, Coluna, a, NovoTabuleiro);
- (Simbolo == a) -> inserirCT(Tabuleiro, NovoTabuleiro)
- ).
-
-gerarTabuleiro([[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],
-[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],
-[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],
-[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~]]).
-
-inserirBase(Tabuleiro, NovoTabuleiro):-
-    inserirBPC(Tabuleiro, Tabuleiro2),
-	inserirBTM(Tabuleiro2, Tabuleiro3),
-	inserirBTM(Tabuleiro3, Tabuleiro4),
-	inserirCT(Tabuleiro4, Tabuleiro5),
-	inserirCT(Tabuleiro5, Tabuleiro6),
-	inserirCT(Tabuleiro6, Tabuleiro7),
-	inserirCT(Tabuleiro7, NovoTabuleiro).
-
-
 /* Execução da lógica sequencial do jogo */
 jogar(Tabuleiro, Misseis) :-
   Misseis > 0,
@@ -182,6 +44,11 @@ jogar(Tabuleiro, Misseis) :-
   NovosMisseis =:= 1 -> ultimoMissel, jogar(NovoTabuleiro, NovosMisseis);
   NovosMisseis =:= 0 -> misseisEsgotados, imprimeTabuleiroReal(NovoTabuleiro), gameOver)
   ).
+
+gerarTabuleiro([[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],
+[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],
+[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],
+[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~],[~,~,~,~,~,~,~,~,~,~,~,~]]).
 
 /* Execução do programa */
 :- initialization(main).
---
+++ util.pl
@@ -0,0 +1,37 @@
+﻿:- module(util, [acertou /0,errou /0,invalido /0,selecione /0,selecaoInvalida /0,misseis /1,ultimoMissel /0,misseisEsgotados/0,gameOver /0,vitoria /0]).
+
+/* Impressões simples */
+
+acertou :-
+  write('ACERTOU!'), nl.
+
+errou :-
+  write('ERROU!'), nl.
+
+invalido :-
+  write('Você já atirou aqui! Atire em outro lugar.'), nl.
+
+selecione :-
+  write('Selecione as coordenadas de onde deseja atirar!'), nl.
+
+selecaoInvalida :-
+  write('Coordenadas inválidas. Tente novamente.'), nl.
+
+misseis(Qtd) :-
+  write('Você ainda tem '), write(Qtd), write(' mísseis.'), nl, nl.
+
+ultimoMissel :-
+  write('Resta apenas um míssel!'), nl, nl.
+
+misseisEsgotados :-
+  write('Seus mísseis acabaram!'), nl, nl.
+
+gameOver :-
+  write('---------------------------------------------------'), nl,
+  write(' Game Over!'), nl,
+  write('---------------------------------------------------').
+
+vitoria :-
+  write('---------------------------------------------------'), nl,
+  write('Você destruiu toda as Bases inimigas !! Parabéns, estrategista!'), nl,
+  write('---------------------------------------------------').
